3f430318df6ef9468252d8f219cfe91005c9be63
andywestley <andy@andrewwestley.co.uk>
2026-01-21 13:33:58 +0000

    Fixed glitch in journey page

--- journeys/index.php
+++ journeys/index.php
@@ -11,16 +11,6 @@ include '../includes/header.php';
     <div class="col-md-6 mb-4">
         <div class="card h-100">
             <div class="card-body">
-include '../includes/header.php';
-?>
-
-<h1>User Journeys</h1>
-<p class="lead">Select a journey below to simulate common user flows. These flows contain intentional accessibility errors for testing purposes.</p>
-
-<div class="row mt-4">
-    <div class="col-md-6 mb-4">
-        <div class="card h-100">
-            <div class="card-body">
                 <h5 class="card-title">Login Flow</h5>
                 <p class="card-text">A standard email and password login form. Issues include poor contrast, missing labels, and lack of autocomplete.</p>
                 <a href="login.php" class="btn btn-primary">Start Login Flow</a>
